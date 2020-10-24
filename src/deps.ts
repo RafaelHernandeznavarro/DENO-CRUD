67b5b9388b41720df1142e5f0159a1cb3afae8f7
rafaelhe <rafael.hernandez@nexum.com>
2020-10-24 23:13:06 +0200

    project files changes

--- src/deps.ts
+++ src/deps.ts
@@ -1,6 +1,6 @@
-export type { RouterMiddleware } from 'https://deno.land/x/oak@v6.3.1/mod.ts'
-
-export { Application, Context, Router, Request, Response, RouteParams } from 'https://deno.land/x/oak@v6.3.1/mod.ts'
+// export type { RouterMiddleware } from 'https://deno.land/x/oak@v6.3.1/mod.ts'
+export {existsSync } from "https://deno.land/std@0.74.0/fs/mod.ts";
+export { Application, Context, Router, Request, Response, RouteParams, send } from 'https://deno.land/x/oak@v6.3.1/mod.ts'
 export * from 'https://deno.land/x/oak@v6.3.1/cookies.ts'
 export * as log from 'https://deno.land/std@0.74.0/log/mod.ts'
 export { v4 } from 'https://deno.land/std@0.74.0/uuid/mod.ts'
